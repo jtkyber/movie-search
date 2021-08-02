0ebbd992648c90414b036e5427dacef5dcda7444
jtkyber <43868043+jtkyber@users.noreply.github.com>
2021-08-01 18:04:32 -0600

    can now press enter to search, button changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useStoreState, useStoreActions } from 'easy-peasy';
 import MovieList from './components/MovieList';
 import ToggleViewIcon from './components/ToggleViewIcon';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
@@ -11,21 +12,30 @@ function App() {
   const [currentSearch, setCurrentSearch] = useState('');
 
   const findMovies = async () => {
-    setCurrentSearch('');
-    try {
-      const res = await fetch(`https://movie-database-imdb-alternative.p.rapidapi.com/?s=${currentSearch}`, {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-key": "387e27d97fmsh8e24e62224ddd7bp139e87jsn4629eff6112a",
-        "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com"
+    if (currentSearch.length) {
+      setCurrentSearch('');
+      try {
+        const res = await fetch(`https://movie-database-imdb-alternative.p.rapidapi.com/?s=${currentSearch}`, {
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-key": "387e27d97fmsh8e24e62224ddd7bp139e87jsn4629eff6112a",
+          "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com"
+        }
+      })
+        const movies = await res.json();
+        setMovieResults(movies.Search);
+      } catch(err) {
+        console.log(err);
       }
-    })
-      const movies = await res.json();
-      setMovieResults(movies.Search);
-    } catch(err) {
-      console.log(err);
     }
   }
+
+  document.addEventListener("keydown", (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      document.querySelector('.enter').click();
+    }
+  })
 
   return (
     <div className='appContainer'>
@@ -34,9 +44,9 @@ function App() {
       </header>
       <div className='grid'>
         <div className='searchBoxContainer'>
-          <input onChange={(e) => setCurrentSearch(e.target.value)} value={currentSearch} className='searchBox' type='text' placeholder='Search for a Movie' />
-          <button onClick={findMovies} className='enter'>Enter</button>
-          <button className='favorites'>Favorites</button>
+          <input onChange={(e) => setCurrentSearch(e.target.value)} value={currentSearch} className='searchBox' type='text' placeholder='Search for a movie, show, game, etc...' />
+          <Button variant="outline-info" onClick={findMovies} className='enter'>Enter</Button>
+          <Button variant="outline-secondary" className='favorites'>Favorites</Button>
           <ToggleViewIcon />
         </div>
         <div className='resultContainer'>
